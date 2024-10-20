de11b91e08efbd4b88eb90f6b0cfcb317f036328
jcuzcan <167156624+jcuzcan@users.noreply.github.com>
2024-10-19 22:59:09 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: '.',  // This ensures Vite uses the current directory as root
+  build: {
+    rollupOptions: {
+      input: './index.html',  // Ensure it knows where your index.html is located
+    },
+  },
 })
